ad5965b0dd673bd6cc81f51f911942f57f51dfda
zeus <emmanuelcach6@gmail.com>
2022-02-21 04:51:20 -0500

    widget alert

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -9,6 +9,19 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title:  Text('Alert Page'),
       ),
+
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Mostrar Alerta'),
+          onPressed: () => _mostrarAlerta(context),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.red,
+            shape: StadiumBorder()
+          ),
+          
+        )
+      ),
+
       floatingActionButton: FloatingActionButton(
          child: Icon(Icons.add_location),
          onPressed: () {
@@ -17,6 +30,40 @@ class AlertPage extends StatelessWidget {
       ),
       
     );
+  }
+
+  void _mostrarAlerta(BuildContext context) {
+      showDialog(
+          context: context,
+          barrierDismissible: true,
+          builder: (context) {
+             return AlertDialog(
+               shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+               title: Text('Titulo'),
+               content: Column(
+                 mainAxisSize: MainAxisSize.min,
+                 children: <Widget>[
+                   Text('Este es el contenido de la caja de la alerta'),
+                   FlutterLogo(size: 100.0)
+                ],
+               ),
+               actions: <Widget>[
+                 ElevatedButton(
+                 child: Text('Cancelar'),
+                 onPressed: ()=> Navigator.of(context).pop(),
+
+                 ),
+
+                 ElevatedButton(
+                 child: Text('Cancelar'),
+                 onPressed: () {
+                  Navigator.of(context).pop();
+                 },
+                 ),
+               ],
+             );
+          }
+      );
   }
 }
 
