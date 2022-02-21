d76033997e8af8430a5e4c45c66cecbbd5501d1f
zeus <emmanuelcach6@gmail.com>
2022-02-21 08:01:54 -0500

    Widget Circle Avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -8,8 +8,33 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title:  Text('Avatar Page'),
-      )
-      
+        actions: <Widget>[
+
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://www.univistatv.com/wp-content/uploads/2021/12/stan-lee.jpg'),
+              radius: 25.0,
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+                child :Text('SL'),
+                backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+
+   body: Center(
+          child: FadeInImage(
+          image: NetworkImage('https://estaticos-cdn.elperiodico.com/clip/3586e3cb-e85c-446a-af08-19f5238a9684_alta-libre-aspect-ratio_default_0.jpg'),
+          placeholder: AssetImage('assets/elgif.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
+      ),
     );
   }
 }
