ea5735e3e227cf81fb3a8183353659c919524ab2
zeus <emmanuelcach6@gmail.com>
2022-02-21 03:06:44 -0500

    Card Widget

---
+++ lib/src/pages/cart_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class CardPage extends StatelessWidget {
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:  Text('Cards'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0) ,
+        children: <Widget>[
+          _cardTipo1()
+
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo1() {
+
+    return Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon( Icons.photo_album, color: Colors.blue),
+            title: Text('Soy el titulsmsi'),
+            subtitle: Text('sjnsnnenrefreiufnroefneirufnernferunfubruberfyberfybeyrfbyerbfybeuf'),
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+                TextButton(
+                  child: Text('Cancelar'),
+                  onPressed: (){},
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
