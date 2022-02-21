7817e3bdfd6e8fdbcf14e86318c80fa9e078fbb9
zeus <emmanuelcach6@gmail.com>
2022-02-21 04:13:32 -0500

    Personalizado el estilo de los cards

--- lib/src/pages/cart_page.dart
+++ lib/src/pages/cart_page.dart
@@ -14,7 +14,28 @@ class CardPage extends StatelessWidget {
         children: <Widget>[
           _cardTipo1(),
           SizedBox(height: 30.0),
-          _cardTipo2()
+          _cardTipo2(),
+           SizedBox(height: 30.0),
+             _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+           SizedBox(height: 30.0),
+             _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+           SizedBox(height: 30.0),
+             _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+           SizedBox(height: 30.0),
+             _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+           SizedBox(height: 30.0),
+             _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+           SizedBox(height: 30.0),
 
         ],
       ),
@@ -24,6 +45,8 @@ class CardPage extends StatelessWidget {
   Widget _cardTipo1() {
 
     return Card(
+      elevation: 10.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
       child: Column(
         children: <Widget>[
           ListTile(
@@ -48,7 +71,8 @@ class CardPage extends StatelessWidget {
 }
 
 Widget _cardTipo2() {
-  return Card(
+
+  final card = Container(
     child: Column(
       children: <Widget>[
         FadeInImage(
@@ -58,15 +82,32 @@ Widget _cardTipo2() {
           height: 250.0,
           fit: BoxFit.cover,
          ),
-        // Image(
-        //   image: NetworkImage('https://photographylife.com/wp-content/uploads/2017/01/Difficult-landscape-photo-to-take.jpg')
-        //  ),
-
+       
           Container(
             padding: EdgeInsets.all(10.0),
             child: Text('Estoy muy cansado pero nimodos')
         )
       ],
+    ),
+  );
+  return Container(
+    decoration: BoxDecoration(
+    borderRadius: BorderRadius.circular(30.0),
+    color: Colors.white,
+    boxShadow: <BoxShadow>[
+      BoxShadow(
+          color: Colors.black26,
+          blurRadius: 10.0,
+          spreadRadius: 2.0,
+          offset: Offset(2.0, 10.0)
+
+            )
+      
+          ]
+        ),
+      child: ClipRRect(
+         borderRadius: BorderRadius.circular(30.0),
+         child: card,
     ),
   );
 }
