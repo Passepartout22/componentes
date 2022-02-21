453fb6f6637fc3f2808efec71647e34287b11cab
zeus <emmanuelcach6@gmail.com>
2022-02-21 02:31:08 -0500

    Rutas dinamicas y archivo de rutas independiente

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:componentes/src/pages/alert_page.dart';
-import 'package:componentes/src/pages/avatar_page.dart';
+import 'package:componentes/src/routes/routes.dart';
 import 'package:flutter/material.dart';
-import 'package:componentes/src/pages/home_page.dart';
+
 
 
 void main() => runApp(MyApp());
@@ -14,10 +14,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       // home: HomePage(),
       initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/' : (BuildContext context) => HomePage(),
-        'alert' : (BuildContext context) => AlertPage(),
-        'avatar' : (BuildContext context) => AvatarPage(),
+      routes: getApplicationRoutes(),
+      onGenerateRoute: (settings){
+
+        return MaterialPageRoute(
+          builder: (context ) => AlertPage()
+          );
       },
     );
   }
