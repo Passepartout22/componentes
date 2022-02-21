56ab99db9fac4529f5bf70dd864d72c6ddcb8eea
zeus <emmanuelcach6@gmail.com>
2022-02-21 03:33:10 -0500

    Image y Fadein Image

--- lib/src/pages/cart_page.dart
+++ lib/src/pages/cart_page.dart
@@ -12,7 +12,9 @@ class CardPage extends StatelessWidget {
       body: ListView(
         padding: EdgeInsets.all(10.0) ,
         children: <Widget>[
-          _cardTipo1()
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2()
 
         ],
       ),
@@ -43,4 +45,28 @@ class CardPage extends StatelessWidget {
       ),
     );
   }
+}
+
+Widget _cardTipo2() {
+  return Card(
+    child: Column(
+      children: <Widget>[
+        FadeInImage(
+          image: NetworkImage('https://photographylife.com/wp-content/uploads/2017/01/Difficult-landscape-photo-to-take.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+          height: 250.0,
+          fit: BoxFit.cover,
+         ),
+        // Image(
+        //   image: NetworkImage('https://photographylife.com/wp-content/uploads/2017/01/Difficult-landscape-photo-to-take.jpg')
+        //  ),
+
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('Estoy muy cansado pero nimodos')
+        )
+      ],
+    ),
+  );
 }
